766cfd3da2042138a44ba91c6a16810c75e3c634
briusova <briusova@gmail.com>
2025-02-24 15:49:45 +0200

    дипломы

--- pages/a7744e73-881a-41f5-9721-a5d3323f00a2.vue
+++ pages/a7744e73-881a-41f5-9721-a5d3323f00a2.vue
@@ -1,7 +1,14 @@
 <template>
-    <el-watermark v-for="(url, i) in srcList">
-    <el-image :src="url" class="size-10" fit="contain"
-        :preview-src-list="srcList" :initial-index="i"></el-image></el-watermark>
+    <div class="container my-24 px-6 m-auto">
+        <div class="grid grid-cols-4 gap-6 md:grid-cols-8 lg:grid-cols-12">
+            <div class="col-span-4 lg:col-span-3 flex justify-center" v-for="(url, i) in srcList">
+                <el-image :src="url" class="h-52 w-auto" fit="contain" :preview-src-list="srcList" :initial-index="i"
+                    lazy>
+                </el-image>
+            </div>
+        </div>
+    </div>
+
 </template>
 
 <script setup>
