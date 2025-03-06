07309869518da00cbc914a890a2306e280117562
briusova <briusova@gmail.com>
2025-03-06 19:29:48 +0200

    библиотэка

--- pages/91ab7a75-4782-47d7-bec5-a73004fe86e0.vue
+++ pages/91ab7a75-4782-47d7-bec5-a73004fe86e0.vue
@@ -1,1 +1,38 @@
-<template></template>
+<template>
+    <section class="py-14" un-cloak>
+        <div class="max-w-screen-xl mx-auto px-4 text-gray-600 md:px-8">
+            <div class="max-w-xl space-y-3 not-prose mb-6">
+                <p class="text-gray-800 text-3xl font-semibold sm:text-4xl">Библиотека</p>
+                <p>Больше статей на сайте.</p>
+            </div>
+
+            <ul class="divide-y divide-slate-100 not-prose">
+                <li v-for="{ header, to, description, images, icon } in the.$children">
+                    <RouterLink :to="to" class="flex items-start gap-4 px-4 py-3">
+                        <div class="flex items-center shrink-0">
+                            <img :src="images[0]?.url" :alt="images[0]?.alt" class="w-40 rounded">
+                        </div>
+                        <icon class="size-6" :icon="icon"></icon>
+                        <div class="flex flex-col gap-0 min-h-[2rem] items-start justify-center w-full min-w-0">
+                            <h4 class="text-base text-slate-700 ">{{ header }}</h4>
+                            <p class="w-full text-sm text-slate-500">{{ description }}</p>
+                        </div>
+                    </RouterLink>
+                </li>
+            </ul>
+            <p></p>
+            <el-button class="not-prose" tag="router-link" :to="the.parent.to" :icon="Back" type="success">на
+                главную</el-button>
+
+        </div>
+
+    </section>
+
+</template>
+
+<script setup>
+import { inject } from "vue";
+const { id } = defineProps(["id"]);
+const pages = inject("pages");
+const the = pages[id];
+</script>
