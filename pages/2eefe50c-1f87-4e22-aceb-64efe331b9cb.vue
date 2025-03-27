981d5bb9bf1a5034b6664e3a90292a8a1e748e2c
briusova <briusova@gmail.com>
2025-03-27 13:30:50 +0200

    случаи

---
+++ pages/2eefe50c-1f87-4e22-aceb-64efe331b9cb.vue
@@ -0,0 +1,22 @@
+<template>
+    <section class="py-14">
+        <div class="max-w-screen-xl mx-auto px-4 text-gray-600 md:px-8">
+            <h1>{{ the.header }}</h1>
+            
+
+            <p>Кто читал,.</p>
+
+            <p>Спасибо, что дочитали до конца, и будьте здоровы!</p>
+            <el-button class="not-prose" tag="router-link" :to="the.parent.to" :icon="Back" type="primary">{{
+                the.parent.header }}</el-button>
+
+        </div>
+    </section>
+</template>
+<script setup>
+import { inject } from "vue";
+import { Back } from '@element-plus/icons-vue';
+const { id } = defineProps(["id"]);
+const pages = inject("pages");
+const the = pages[id];
+</script>
--- pages/a5260e28-9f5f-468e-9c9a-448befe6e5fb.vue
+++ pages/a5260e28-9f5f-468e-9c9a-448befe6e5fb.vue
@@ -37,12 +37,44 @@
         </p>
         <p></p>
 
+
+    <section class="py-14" un-cloak>
+        <div class="max-w-screen-xl mx-auto px-4 text-gray-600 md:px-8">
+  
+
+            <ul class="divide-y divide-slate-100 not-prose">
+                <li v-for="{ header, to, images } in the.$children">
+                    <RouterLink :to="to" class="flex items-start gap-4 px-4 py-3">
+                        <div class="flex items-center shrink-0">
+                            <img :src="images[0]?.url" :alt="images[0]?.alt" class="w-20 rounded">
+                        </div>
+                        <div class="flex flex-col gap-0 min-h-[2rem] items-start justify-center w-full min-w-0">
+                            <h4 class="text-base text-slate-700 ">{{ header }}</h4>
+                            <p class="w-full text-sm text-slate-500">{{ description }}</p>
+                        </div>
+                    </RouterLink>
+                </li>
+            </ul>
+
+
+        </div>
+    </section>
+
+
+
+
+
+
+
+
+
         <el-button class="not-prose" tag="router-link" :to="the.parent.to" :icon="Back" type="success">на
             главную</el-button>
 
 
     </div>
 </template>
+
 
 <script setup>
 import { inject } from "vue";
