7dfa889d00c428248ef5a14481c7953a7ea2ff91
briusova <briusova@gmail.com>
2025-03-29 09:38:51 +0200

    один размер

--- pages/1dc71d11-d8fb-4223-a7ac-9a146ee8035d.vue
+++ pages/1dc71d11-d8fb-4223-a7ac-9a146ee8035d.vue
@@ -1,18 +1,33 @@
 <template>
+    <div class="ui container">
 
-    <div class="prose max-w-none text-slate-500 mb-10">
         <h1 class="mt-12" style="text-align: left;">Мои статьи и публикации</h1>
+     
+        
        
-    </div>
+       
+   
+       
 
 
-    <div class="my-4 flex flex-col items-start gap-4 sm:flex-row animate__animated animate__faster"
+
+
+<div class="not-prose my-16">
+ <div class="my-4 flex flex-col items-start gap-4 sm:flex-row animate__animated animate__faster"
         v-for="({ images, to, title, description, icon }, i) in $children"
         :class="{ animate__fadeInRight: fade[i], animate__fadeOutRight: !fade[i], }"
         v-intersection-observer="([{ isIntersecting }]) => { fade[i] = isIntersecting }">
-        <router-link :to="to">
-            <img :src="images[0].url"
-                class="flex flex-auto w-full h-auto sm:w-48 bg-center bg-cover overflow-hidden rounded"></router-link>
+        <div @mouseover="slide[i] = true" @mouseleave="slide[i] = false" :class="`bg-[url(${images[0].url})]`"
+            class="flex flex-auto w-full h-48 sm:w-48 sm:h-24 bg-center bg-cover overflow-hidden rounded">
+            <transition enter-active-class="animate__animated animate__slideInUp animate__faster"
+                leave-active-class="animate__animated animate__slideOutUp  animate__faster">
+                <router-link v-if="slide[i]" class="bg-white/85 flex-auto flex justify-center items-center" :to="to">
+                    <el-button size="large" circle="" tag="router-link" :to="to">
+                        <icon :icon="icon"></icon>
+                    </el-button>
+                </router-link>
+            </transition>
+        </div>
         <router-link :to="to" class="flex w-full min-w-0 gap-4 text-base" @mouseover="mouseenter[i] = true"
             @mouseout="mouseenter[i] = false">
             <icon :icon="icon" class="size-10 animate__animated animate__slow text-emerald-500"
@@ -24,21 +39,28 @@
                 <p class="text-slate-500">{{ description }}</p>
             </div>
         </router-link>
+    </div>
+</div>
 
+
+
+
+
+
+
+
+
+
+
+        <el-button class="not-prose" tag="router-link" :to="parent.to" :icon="Back" type="success">на
+            главную</el-button>
 
 
     </div>
-
-    <el-button class="not-prose" tag="router-link" :to="parent.to" :icon="Back" type="success">на
-        главную</el-button>
 </template>
-
-
-
 <script setup>
-import { inject, ref, watch } from "vue";
 import { Back } from '@element-plus/icons-vue';
-
+import { inject, ref, watch } from "vue";
 import { vIntersectionObserver } from "@vueuse/components";
 
 const { id } = defineProps(["id"]),
@@ -50,4 +72,5 @@ const { id } = defineProps(["id"]),
     deep = true;
 
 watch(slide, () => { __unocss_runtime.extract("bg-white/85") }, { deep, once });
+
 </script>
